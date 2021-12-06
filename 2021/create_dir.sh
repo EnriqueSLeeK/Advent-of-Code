66739530bee7c2ab13891d3db2e5d1e55b62bf42
Pew_Booom <ghistyo@gmail.com>
2021-12-06 16:19:54 -0300

    create_dir update

--- 2021/create_dir.sh
+++ 2021/create_dir.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+echo "Before using please copy your input to the system clipboard"
+
 #Basic script to create a dir and files
 echo "Input day:"
 read day
 
 mkdir "$day day"
 mkdir "$day day/phase_1"
-> "$day day/phase_1/input"
-> "$day day/phase_1/solver.py"
+xclip -o -sel clip > "$day day/phase_1/input"
+echo 'with open("input", "r") as f:' > "$day day/phase_1/solver.py"
 mkdir "$day day/phase_2"
-> "$day day/phase_2/input"
-> "$day day/phase_2/solver.py"
+xclip -o -sel clip > "$day day/phase_2/input"
+echo 'with open("input", "r") as f:' > "$day day/phase_2/solver.py"
